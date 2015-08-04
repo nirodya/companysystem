fcd445f4283104f25d15c96605803c949e5a45e3
Nirodya <ACTOP@ACTOP-PC>
2015-08-04 18:38:00 +0530

    Merge origin/master
    Conflicts:
    	ActopCompanySystem/src/java/com/actop/model/UserManagement.java
    	ActopCompanySystem/web/WEB-INF/web.xml

--- ActopCompanySystem/src/java/com/actop/taglibs/RetrieveLeaveApproval.java
+++ ActopCompanySystem/src/java/com/actop/taglibs/RetrieveLeaveApproval.java
@@ -3,22 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.actop.taglibs;
 
+import com.actop.db.DepartmentsHasDesignation;
+import com.actop.db.Employers;
+import com.actop.db.LeaveApproval;
+import com.actop.db.UserLogin;
+import com.actop.model.ApprovalManagement;
+import com.actop.model.UserManagement;
+import java.util.List;
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.TagSupport;
 
 /**
  *
  * @author Nirodya Gamage
  */
-public class RetrieveLeaveApproval extends TagSupport{
+public class RetrieveLeaveApproval extends TagSupport {
 
     @Override
     public int doStartTag() throws JspException {
-        
-        return super.doStartTag(); //To change body of generated methods, choose Tools | Templates.
+        ApprovalManagement am = new ApprovalManagement();
+        UserManagement um = new UserManagement();
+        UserLogin sessul = (UserLogin) pageContext.getAttribute("loggedUser", PageContext.SESSION_SCOPE);
+        Employers e = sessul.getEmployers();
+        DepartmentsHasDesignation dhd = um.getDepartmentHasDesignation(e);
+        List<LeaveApproval> las = am.checkLeaveApproval(dhd);
+        if (las.size() > 0) {
+            pageContext.setAttribute("leaveApprovals", las);
+            return EVAL_BODY_INCLUDE;
+        } else {
+            return SKIP_BODY; //To change body of generated methods, choose Tools | Templates.
+        }
     }
 
 }
